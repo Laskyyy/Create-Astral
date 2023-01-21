2620acb0c939e11ee34b61b9be41e14d628d1f49
Jake <jakegames100@gmail.com>
2023-01-21 15:30:37 +0000

    Cobalt ore below y=30

--- kubejs/startup_scripts/startup.js
+++ kubejs/startup_scripts/startup.js
@@ -1175,6 +1175,21 @@ onEvent('worldgen.add', event => {
 			.triangleHeight(0, 85)
 	})
 
+    event.addOre((ore) => {
+		ore.id = "kubejs:cobaltoremoon" // optional
+		ore.biomes = [{
+			not: {
+				category: "river"
+			}
+		}]
+		ore.addTarget('#ad_astra:moon_ore_replaceables', 'tconstruct:cobalt_ore')
+
+		ore.count([10, 20])
+			.squared()
+			.triangleHeight(0, 30)
+      
+	})
+
   event.addOre((ore) => {
 		ore.id = "kubejs:silveroremoon" // optional
 		ore.biomes = ['ad_astra:lunar_wastelands', 'minecraft:basalt_deltas', 'incendium:toxic_heap']
