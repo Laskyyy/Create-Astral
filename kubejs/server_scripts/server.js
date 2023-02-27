4870b1e80831a0e8f30ab55f7279ed4468a18903
Jake <jakegames100@gmail.com>
2023-02-27 16:19:38 +0000

    Remove Lunarian Wanderers + Remove Modded villager trades

--- kubejs/server_scripts/server.js
+++ kubejs/server_scripts/server.js
@@ -3698,3 +3698,9 @@ onEvent('player.logged_in', event => {
     event.player.give('ftbquests:book')
   }
 })
+
+
+onEvent("morejs.villager.trades", (event) => {
+  event.removeModdedTrades();
+  event.removeModdedTrades([...professions], level);
+});
