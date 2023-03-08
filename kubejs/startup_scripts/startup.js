1f6f0fc0386fd7688f124be6bb51a24fe29d43a2
Druivenpasta <94719433+Druivenpasta@users.noreply.github.com>
2023-03-08 10:41:10 +0100

    Update tooltips

--- kubejs/startup_scripts/startup.js
+++ kubejs/startup_scripts/startup.js
@@ -480,6 +480,10 @@ onEvent('block.registry', event => {
 	 
 	 
 onEvent('item.tooltip', e => {
+  e.addAdvanced('tconstruct:smeltery_controller', (item, advanced, text) => {
+      text.add(Text.of('disabled, use a foundry instead').red())
+    }
+  })
   e.addAdvanced('techreborn:lead_ingot', (item, advanced, text) => {
     if (!e.isShift()) {
       text.add(1, [Text.of('Hold ').gold(), Text.of('Shift ').yellow(), Text.of('to see more info.').gold()])
