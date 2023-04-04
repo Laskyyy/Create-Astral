deacbbd134b726efb67c909a84d155d965f71c11
arc25275 <arc25275@gmail.com>
2023-04-03 22:54:55 -0700

    Slow down electrolyser recipe, polish it a bit

--- kubejs/client_scripts/ponder.js
+++ kubejs/client_scripts/ponder.js
@@ -41,6 +41,11 @@ onEvent("ponder.registry", (event) => {
                     [1, 2, 1],
                     [3, 4, 3],
                 ];
+                scene.world.setBlock(
+                    [3, 4, 0],
+                    "ad_astra:oxygen_loader",
+                    false
+                );
                 scene.world.modifyBlock(
                     [2, 2, 0],
                     () =>
@@ -52,7 +57,19 @@ onEvent("ponder.registry", (event) => {
                             .with("down", "false"),
                     false
                 );
-
+                scene.world.modifyBlock(
+                    [2, 1, 1],
+                    () =>
+                        Block.id("create:fluid_pipe")
+                            .with("south", "false")
+                            .with("north", "false")
+                            .with("east", "false")
+                            .with("west", "true")
+                            .with("up", "true")
+                            .with("down", "false"),
+                    false
+                );
+                scene.showBasePlate();
                 for (
                     let y = multiblockRange[0][1];
                     y <= multiblockRange[1][1];
@@ -71,7 +88,7 @@ onEvent("ponder.registry", (event) => {
                             scene.world.showSection([x, y, z], Facing.DOWN);
                         }
                     }
-                    scene.idle(10);
+                    scene.idle(30);
                     if (y == 3) {
                         scene
                             .text(30, "Leave a gap in the center", [2, 4, 2])
@@ -90,22 +107,23 @@ onEvent("ponder.registry", (event) => {
                     .placeNearTarget()
                     .attachKeyFrame();
 
-                for (let y = 0; y <= 1; y++) {
-                    for (let x = 0; x <= 4; x++) {
-                        for (let z = 0; z <= 2; z++) {
-                            scene.world.showSection([x, y, z], Facing.UP);
-                        }
+                for (let x = 0; x <= 4; x++) {
+                    for (let z = 0; z <= 2; z++) {
+                        scene.world.showSection([x, 1, z], Facing.EAST);
                     }
+                }
+                for (let z = 0; z <= 4; z++) {
+                    scene.world.showSection([0, 2, z], Facing.EAST);
                 }
                 scene.idle(40);
                 scene
                     .text(
-                        60,
+                        30,
                         "Pump oxygen out of the side of the Electrolyser ",
                         electrolyserPos2
                     )
                     .attachKeyFrame();
-                for (let y = 2; y <= 4; y++) {
+                for (let y = 2; y <= 3; y++) {
                     for (let x = 0; x <= 4; x++) {
                         scene.world.showSection([x, y, 0], Facing.SOUTH);
                     }
@@ -113,8 +131,19 @@ onEvent("ponder.registry", (event) => {
                 }
                 scene.idle(10);
                 scene.particles
-                    .simple(1000, "ad_astra:oxygen_bubble", [3, 5, -0.5])
+                    .simple(60, "ad_astra:oxygen_bubble", [2.7, 5, -0.5])
                     .gravity(-0.1);
+                scene.idle(120);
+                scene.world.showSection([3, 4, 0], Facing.DOWN);
+
+                scene.idle(10);
+                scene
+                    .text(
+                        100,
+                        "Oxygen pumped into the Oxygen Loader can be used to fill up your spacesuits and tanks",
+                        [3, 4.5, 0]
+                    )
+                    .placeNearTarget();
             }
         );
     event.printParticleNames();
