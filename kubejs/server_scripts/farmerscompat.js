495196a802b370bbdccf987a5f967258016a842d
RandomUser240306 <48233384+RandomUser240306@users.noreply.github.com>
2023-04-10 22:37:11 -0500

    Update farmerscompat.js

--- kubejs/server_scripts/farmerscompat.js
+++ kubejs/server_scripts/farmerscompat.js
@@ -2,32 +2,26 @@ onEvent("recipes", (event) => {
     mixingRecipeGen(event);
 })
 
-var FULL_BUCKET_AMMOUNT = 81000;
-var INGOT_FLUID_AMMOUNT = 9000;
+const FULL_BUCKET_AMOUNT = 81000;
+const INGOT_FLUID_AMOUNT = 9000;
 
 function mixingRecipeGen(event) {
     event.forEachRecipe({type:"farmersdelight:cooking"}, recipe => {
         //Gets item name without namespace (e.g. "cooked_rice")
-        var outputItem = recipe.getOriginalRecipeResult().getItem().getId().substring(recipe.getOriginalRecipeResult().getItem().getId().indexOf(":")+1); 
+        var outputItem = recipe.getOriginalRecipeResult().getId().split(":")[1]; 
         var inputItems = recipe.getOriginalRecipeIngredients();
-        var container;
-        
-        //Determines container for spouting recipe
-        if(outputItem=="hot_cocoa" | outputItem=="apple_cider" | outputItem=="hamburger") {
-            container="minecraft:glass_bottle";
-        } else if(outputItem=="stuffed_pumpkin_block") {
-            container="minecraft:pumpkin";
-        } else if(outputItem=="dumplings") {
-            container="#c:dough";
-        } else {
-            container="minecraft:bowl";
+        let containers = {
+            "minecraft:glass_bottle": [ "hot_cocoa","apple_cider","hamburger"],
+            "minecraft:pumpkin": ["stuffed_pumpkin_block"]
         }
+
+        var container = Object.keys(containers).find(key => containers[key].includes(outputItem)) ?? "minecraft:bowl";
         
         //Replaces tomato sauce bowl with fluid in recipes and takes dough out of dumpling recipe
         for (let i = 0; i < inputItems.length; i++) {
             if (inputItems[i].getFirst() == "farmersdelight:tomato_sauce") {
                 inputItems.remove(i);
-                inputItems.push({fluid:"kubejs:tomato_sauce_fluid", amount: FULL_BUCKET_AMMOUNT/4});
+                inputItems.push({fluid:"kubejs:tomato_sauce_fluid", amount: FULL_BUCKET_AMOUNT/4});
                 break;
             } else {
                 inputItems[i].getFirst().getTags().forEach( tag => {
@@ -37,7 +31,7 @@ function mixingRecipeGen(event) {
         }
         //Generates mixing recipe
         event.recipes
-            .createMixing({ fluid: "kubejs:" + outputItem + "_fluid", amount: FULL_BUCKET_AMMOUNT/4 }, inputItems)
+            .createMixing({ fluid: "kubejs:" + outputItem + "_fluid", amount: FULL_BUCKET_AMOUNT/4 }, inputItems)
             .heatRequirement("heated")
             .processingTime(100);
         
@@ -45,7 +39,7 @@ function mixingRecipeGen(event) {
         event.recipes
             .createFilling(recipe.getOriginalRecipeResult(), [
             container,
-            { fluid: "kubejs:" + outputItem + "_fluid", amount: FULL_BUCKET_AMMOUNT/4 },
+            { fluid: "kubejs:" + outputItem + "_fluid", amount: FULL_BUCKET_AMOUNT/4 },
         ]);
         
     });  
