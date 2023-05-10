d0fc7b10194e25b1e11a7ec1ab56460e6a0fb0d1
Sam <66283189+Not-Roy@users.noreply.github.com>
2023-05-10 16:46:26 -0400

    Fix Step Up II not working as intended

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -44,17 +44,23 @@ onEvent('rei.add.items', event => {
   })
 
 function getPlayerStepHeight(player) {
+  const DEFAULT_STEP_HEIGHT = 0.6;
+
   const leggingsTag = player.getLegsArmorItem().toNBT().tag;
-  if (leggingsTag === undefined) return 0.6;
+  if (leggingsTag === undefined) return DEFAULT_STEP_HEIGHT;
 
   const leggingsModifiers = leggingsTag.tic_modifiers;
-  if (leggingsModifiers === undefined) return 0.6;
+  if (leggingsModifiers === undefined) return DEFAULT_STEP_HEIGHT;
 
   const stepUp = leggingsModifiers.filter(mod => mod.name == "tconstruct:step_up")[0];
-  if (stepUp !== undefined && stepUp.level > 0) {
+  if (stepUp === undefined) return DEFAULT_STEP_HEIGHT;
+
+  if (stepUp.level == 2) {
+    return 1.6;
+  } else if (stepUp.level == 1) {
     return 1.1;
   } else {
-    return 0.6;
+    return DEFAULT_STEP_HEIGHT;
   }
 }
 
