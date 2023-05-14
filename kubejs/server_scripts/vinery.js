c63293bb148f75c0ce0733116465a09726137f18
HyperRaccoon13 <68512889+HyperRaccoon13@users.noreply.github.com>
2023-05-14 19:18:36 +0100

    Fixed vinery wine rack
    The recipe needed a oak trap door, 
    just changed that to be any wooden trap door

--- kubejs/server_scripts/vinery.js
+++ kubejs/server_scripts/vinery.js
@@ -13,3 +13,13 @@ onEvent('entity.loot_tables', event => {
       })
     })
 })
+
+onEvent('recipes', event => {
+  event.remove({output: 'vinery:wine_rack_1'})
+
+  event.shaped("vinery:wine_rack_1", ["PSP", "STS", "PSP"], {
+      P: "#planks",
+      S: "#wooden_slabs",
+      T: "#wooden_trapdoors",
+  })
+})
