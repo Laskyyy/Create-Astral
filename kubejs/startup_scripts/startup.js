60ae0aedfa574708c415e795590ede12d2baa2b7
GreenOxygen <88381570+TheRealGreenOxygen@users.noreply.github.com>
2023-06-01 18:02:30 +0200

    Enchantment Assembly (#150)
    * Add files via upload
    
    W.I.P.
    Feel free to make it prettier
    
    * Add files via upload
    
    W.I.P.
    Feel Free to exchange for a better one
    
    * Add files via upload
    
    Simply the Integration of Ingot/Block/Liquid of exp
    
    * Add files via upload
    
    The Necessary changes for consistency, regarding xp Nuggies
    
    * resprite
    
    * Add files via upload
    
    My magnum opus
    
    This is the core of the Enchantment Assembly
    
    * Update startup.js
    
    * Update startup.js
    
    * Update startup.js
    
    * Update tconstruct.js
    
    * Update tconstruct.js
    
    * Update create.js
    
    last couple of bits
    
    * Conflicts should be resolved
    
    * okay, now they should
    
    * Revert "okay, now they should"
    
    This reverts commit cad10bfd5f7433adcb6411ccc8dbe90df5f10bad.
    
    * Revert "Conflicts should be resolved"
    
    This reverts commit 9df7b1b848344652c97aa95c113c03c60e318f9f.
    
    ---------
    
    Co-authored-by: Xelo <williamchang435@gmail.com>
    Co-authored-by: Laskyyy 
    Co-authored-by: Laskyyy

--- kubejs/startup_scripts/startup.js
+++ kubejs/startup_scripts/startup.js
@@ -104,7 +104,15 @@ event.create('aurorite')
        .thinTexture(0xEEEDC4)
        .bucketColor(0xEEEDC4)
        .displayName('White Grape Juice')
-	liquidFoodGen(event)
+	
+event.create('liquid_xp_nuggies')
+       .thickTexture(0x17ff45)
+       .bucketColor(0x17ff45)
+       .displayName('Liquified Experience Nuggets')
+       .stillTexture('tconstruct:block/fluid/molten/shimmer')
+       .flowingTexture('tconstruct:block/fluid/molten/shimmer')
+  
+liquidFoodGen(event)
 })
 
 //LIQUID FOODS
@@ -419,6 +427,12 @@ onEvent('block.registry', event => {
       .hardness(4)
       .tagBlock('minecraft:mineable/pickaxe')
       .material('stone')    
+
+  event.create('createastral:experience_block')
+      .tagBlock('minecraft:mineable/pickaxe')
+      .material('glass')    
+      .hardness(2)
+      .lightLevel(5)
  })
 
  ///// ADD BURN TIME TO FUEL /////
@@ -560,6 +574,7 @@ event.create('createastral:shimmer_amplifier'),
 event.create('createastral:star_shard'),
 event.create('createastral:pure_star_shard').glow(true),
 event.create('createastral:crushed_yttrium_ore'),
+event.create('createastral:experience_ingot'),
 
 
 
