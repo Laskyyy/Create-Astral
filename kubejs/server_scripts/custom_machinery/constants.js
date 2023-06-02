4729f8ce7263f5f920afee99aa4f24f019ac35da
ZZZank <3410764033@qq.com>
2023-06-02 21:38:13 +0800

    try to build electrolyser recipes using kubejs

---
+++ kubejs/server_scripts/custom_machinery/constants.js
@@ -0,0 +1,24 @@
+//priority: 101
+
+const structureCustomElectrolyser = `{
+    "type": "custommachinery:structure",
+    "keys": {
+        "a": "create:copper_casing",
+        "b": "createastral:copper_heating_coil"
+    },
+    "pattern": [
+        [
+            "aaa",
+            "aaa",
+            "ama"
+        ], [
+            "bbb",
+            "b b",
+            "bbb"
+        ], [
+            "aaa",
+            "aaa",
+            "aaa"
+        ]
+    ]
+}`;
---
+++ kubejs/server_scripts/custom_machinery/electrolyser.js
@@ -0,0 +1,44 @@
+
+
+onEvent("server.datapack.first", (event) => {
+    const recipes = [
+        {
+            time: 20,
+            input: {
+                "type": "custommachinery:fluid",
+                "fluid": "minecraft:water",
+                "amount": 8100,
+            },
+            output: {
+                "type": "custommachinery:fluid",
+                "fluid": "ad_astra:oxygen",
+                "amount": 860,
+            }
+        }
+    ]
+
+    var recipeCounter=0;
+    recipes.forEach((recipe) => {
+        let recipe_json = `{
+            "type": "custommachinery:custom_machine",
+            "machine": "createastral:electrolyser",
+            "time": ${recipe.time},
+            "requirements": [
+                ${structureCustomElectrolyser},
+                {
+                    "type": ${recipe.input.type},
+                    "fluid": ${recipe.input.fluid},
+                    "amount": ${recipe.input.amount},
+                    "mode": "input"
+                },
+                {
+                    "type": ${recipe.output.type},
+                    "fluid": ${recipe.output.fluid},
+                    "amount": ${recipe.output.amount},
+                    "mode": "output"
+            }`;
+
+        event.addJson(`cm_elcetrolyser_${recipeCounter}`, recipe_json);
+        recipeCounter+=1;
+    });
+});
