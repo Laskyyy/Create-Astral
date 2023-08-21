5d8d3575f175163e321a91fa3f8ba90f176f6666
Infern0_b0b <nate.brick@hotmail.com>
2023-08-21 14:20:53 -0700

    Recipe to turn 'Refined Radiance Blocks' back into 'Refined Radiance'.

--- kubejs/server_scripts/server.js
+++ kubejs/server_scripts/server.js
@@ -827,6 +827,12 @@ onEvent("recipes", (event) => {
     event.shaped("createastral:refined_radiance_block", ["AAA", "AAA", "AAA"], {
         A: "create:refined_radiance",
     });
+
+    // I added this recipe to turn refined radiance blocks back into refined radiance
+    event.shapeless("9x create:refined_radiance",
+        ["createastral:refined_radiance_block"], {
+    });
+    
     event.shaped("16x create:refined_radiance_casing", ["BBB", "BAB", "BBB"], {
         A: "create:refined_radiance",
         B: "#minecraft:planks",
