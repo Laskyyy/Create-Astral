c3109dedbd17a27bda3cdbd7dc26669059b28397
The Spokesman <94719433+Druivenpasta@users.noreply.github.com>
2023-10-23 17:12:21 +0200

    Asurine + Ochrum

--- kubejs/server_scripts/rock_and_stone/earlygame.js
+++ kubejs/server_scripts/rock_and_stone/earlygame.js
@@ -13,15 +13,15 @@ onEvent("recipes", event => {
         "result": "create:veridium",
         "cooling_time": 60
       })
-    event.recipes.createCrushing("create:veridium",
-     [Item.of("minecraft:raw_copper").withChance(0.3), 
-     Item.of("minecraft:clay_ball").withChance(0.25), 
-     Item.of("techreborn:olivine_dust").withChance(0.5)
-    ])
-    event.recipes.createMilling("create:veridium", [
-        Item.of("minecraft:raw_copper").withChance(0.15),
-        Item.of("minecraft:clay_ball").withChance(0.15)
-    ])
+    //veridium - outputs - base
+    event.recipes.createCrushing([
+    Item.of("minecraft:raw_copper").withChance(0.3), 
+    Item.of("minecraft:clay_ball").withChance(0.25), 
+    Item.of("techreborn:olivine_dust").withChance(0.5)
+   ], "create:veridium")
+    event.recipes.createMilling([
+    Item.of("minecraft:raw_copper").withChance(0.15),
+    Item.of("minecraft:clay_ball").withChance(0.15)], "create:veridium")
     //upgraded method
     event.recipes.createMixing(Item.of("2x create:veridium"), [
         "techreborn:peridot",
@@ -35,5 +35,51 @@ onEvent("recipes", event => {
         "techreborn:olivine_dust",
         Fluid.of("minecraft:lava", 200)
     ])
-
+    //ochrum - base method
+    event.recipes.createCompacting(Item.of("create:ochrum"), [
+        Item.of("4x minecraft:sand"),
+        "create:limestone",
+        "techreborn:pyrite_dust"
+    ])
+    event.recipes.createSplashing(Item.of("techreborn:pyrite_dust").withChance(0.3), "minecraft:sand")
+    event.recipes.createCrushing([
+        Item.of("minecraft:raw_gold").withChance(0.15),
+        Item.of("techreborn:pyrite_dust").withChance(0.4),
+        Item.of("techreborn:limestone_dust").withChance(0.6)
+    ], "create:ochrum")
+    event.recipes.createMilling([
+        Item.of("minecraft:raw_gold").withChance(0.1),
+        Item.of("techreborn:granite_dust").withChance(0.3)
+    ], "create:ochrum")
+    //ochrum - upgraded method
+    event.recipes.createCompacting(Item.of("2x create:ochrum"), [
+        Item.of("2x create:limestone"),
+        Item.of("6x minecraft:pyrite_dust"),
+        Fluid.of("kubejs:shimmer", 100)
+    ])
+    //asurine - base method
+    //time is intentional - gotta deal with logistics in this one (100 secs btw)
+    event.custom({
+        "type": "tconstruct:casting_basin",
+        "cast": {
+          "item": "minecraft:seared_stone"
+        },
+        "cast_consumed": true,
+        "fluid": {
+          "tag": "minecraft:water",
+          "amount": 1000
+        },
+        "result": "tconstruct:seared_brick",
+        "cooling_time": 2000
+      })
+    //asurine - outputs - base
+    event.recipes.createCrushing([
+        Item.of("techreborn:raw_tin").withChance(0.2),
+        Item.of("techreborn:lazurite_dust").withChance(0.3),
+        Item.of("minecraft:clay_ball").withChance(0.4)
+    ], "create:asurine")
+    event.recipes.createMilling([
+        Item.of("techreborn:raw_tin").withChance(0.1),
+        Item.of("minecraft:clay_ball").withChance(0.2)
+    ], "create:asurine")
 })
