ad4d2989d99ddc0027f98591379d77377ca89d24
Infern0_b0b <nate.brick@hotmail.com>
2023-11-03 13:33:33 -0700

    made crafting recipes for the 4 vanilla horse armors. I redid the leather one as it already had a recipe. I also removed the melting recipe for them as that would make iron, gold, and diamonds dupable if i didn't, based on the recipe i made. They are still crushable.

--- kubejs/server_scripts/server.js
+++ kubejs/server_scripts/server.js
@@ -835,6 +835,30 @@ onEvent("recipes", (event) => {
 
     /// end sifter
 
+    // horse armor recipes
+
+    event.shaped("minecraft:leather_horse_armor", ["  A", "AAA", "ABA"], {
+        A: "minecraft:leather",
+        B: "minecraft:string",
+    });
+    event.shaped("minecraft:iron_horse_armor", ["  A", "ABA", "ACA"], {
+        A: "create:iron_sheet",
+        B: "minecraft:leather_horse_armor",
+        C: "minecraft:string",
+    });
+    event.shaped("minecraft:golden_horse_armor", ["  A", "ABA", "ACA"], {
+        A: "create:golden_sheet",
+        B: "minecraft:iron_horse_armor",
+        C: "minecraft:string",
+    });
+    event.shaped("minecraft:diamond_horse_armor", ["  A", "ABA", "ACA"], {
+        A: "minecraft:diamond",
+        B: "minecraft:golden_horse_armor",
+        C: "minecraft:string",
+    });
+
+    
+
     //// ASSORTED CRAFTING BENCH RECIPES
 
     //dispenser recipe
