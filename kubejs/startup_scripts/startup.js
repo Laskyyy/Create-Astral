908670a963c67dfa5e7f9b57dfa5011761e4ab42
KonSola5 <125081901+KonSola5@users.noreply.github.com>
2024-01-31 20:40:57 +0100

    Add custom drawer upgrades (#269)
    * Modify .gitignore
    
    * Once again
    
    * Custom drawer upgrades
    
    * Hide default upgrades from REI
    
    * Remove incorrectly included files and maybe correct `.gitignore`
    
    * Roll back `.gitignore`

--- kubejs/startup_scripts/startup.js
+++ kubejs/startup_scripts/startup.js
@@ -371,7 +371,6 @@ onEvent("block.registry", (event) => {
         .hardness(3)
         .displayName("Copper Plating")
         .tagBlock("minecraft:mineable/pickaxe");
-
     event
         .create("createastral:refined_radiance_block")
         .material("lantern")
@@ -777,6 +776,35 @@ onEvent("item.registry", (event) => {
         .displayName("coin");
 });
 
+// Java reflection stuff //
+// Custom drawer upgrades
+
+const $DeferredRegister = java('dev.architectury.registry.registries.DeferredRegister')
+const $Tiers = java('net.minecraft.world.item.Tiers')
+const $Item = java('net.minecraft.world.item.Item')
+const $Registry = java('net.minecraft.core.Registry')
+const $UpgradeItem = java('io.github.mattidragon.extendeddrawers.item.UpgradeItem')
+const $ResourceLocation = java('net.minecraft.class_2960')
+const $FabricItemSettings = java('net.fabricmc.fabric.api.item.v1.FabricItemSettings')
+
+let ITEMS = $DeferredRegister.create('createastral', $Registry.ITEM_REGISTRY)
+let drawer_multipliers = {} // This will be used later for adding tooltips.
+
+function registerUpgrade(mod_id, name, multiplier) {
+    drawer_multipliers[`${mod_id}:${name}`] = multiplier;
+    ITEMS['register(net.minecraft.class_2960,java.util.function.Supplier)'](`${mod_id}:${name}`, () => {
+        return new $UpgradeItem(new $FabricItemSettings(), $ResourceLocation(mod_id, `item/${name}`), multiplier)
+    })
+}
+
+registerUpgrade('createastral', 't1_upgrade', 2)
+registerUpgrade('createastral', 't2_upgrade', 4)
+registerUpgrade('createastral', 't3_upgrade', 16)
+registerUpgrade('createastral', 't4_upgrade', 64)
+registerUpgrade('createastral', 't5_upgrade', 256)
+registerUpgrade('createastral', 't6_upgrade', 1048576) // 2^20x, this makes the regular drawer store 2^31 items
+ITEMS.register()
+
 ///// ITEM TOOLTIPS REGISTRY /////
 
 onEvent("item.tooltip", (e) => {
@@ -1045,7 +1073,8 @@ onEvent("item.tooltip", (e) => {
             item: "adoptafloppa:amazon_delivery",
             tooltip:
                 'A note is attached: "Feed this thing fish once a day or else it will explode. Good luck.".',
-        },
+        }
+        
     ];
 
     tooltips.forEach(tooltip => {
@@ -1068,6 +1097,10 @@ onEvent("item.tooltip", (e) => {
       text.add(2, [Text.of('A major change you *might* want to be aware of. The nether does not exist. Do not even try to make the portal as it will not function. All nether related items are distributed throughout the pack (mostly planets)').white(),])
         }
       });
+      // Custom drawer upgrades
+      for (const [item, mutliplier] of Object.entries(drawer_multipliers)) {
+        e.add([item], Text.gray('Drawer Slot capacity: ').append(Text.green(`${mutliplier}x`)))
+      }
 });
 
 ///// CUSTOM ASTRAL WORLDGEN /////
