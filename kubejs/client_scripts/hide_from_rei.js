977a34cabf87211402774d874e33ecee7f78327e
KonSola5 <125081901+KonSola5@users.noreply.github.com>
2024-02-04 10:28:02 +0100

    Some REI hiding and tooltip changes

--- kubejs/client_scripts/hide_from_rei.js
+++ kubejs/client_scripts/hide_from_rei.js
@@ -1,5 +1,15 @@
-const HIDDEN_ITEMS = ["extended_drawers:t1_upgrade", "extended_drawers:t2_upgrade", "extended_drawers:t3_upgrade", "extended_drawers:t4_upgrade", "extended_drawers:creative_upgrade"];
+const HIDDEN_ITEMS = [
+    "extended_drawers:t1_upgrade",
+    "extended_drawers:t2_upgrade",
+    "extended_drawers:t3_upgrade",
+    "extended_drawers:t4_upgrade",
+    "extended_drawers:creative_upgrade",
+];
 
-onEvent("rei.hide.items", event => {
-    HIDDEN_ITEMS.forEach(id => event.hide(id));
+onEvent("rei.hide.items", (event) => {
+    HIDDEN_ITEMS.forEach((id) => event.hide(id));
+});
+
+onEvent("rei.add.items", (event) => {
+    event.add(Item.of("ae2:facade", '{item:"minecraft:stone"}'));
 });
