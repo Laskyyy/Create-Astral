acb7621989203a00d9dda93cb733d26717dac174
Max Norfolk <mnorfolk@psu.edu>
2024-06-30 15:02:57 -0400

    moved swift andesite code to interaction.js

--- kubejs/server_scripts/script.js
+++
@@ -1,6 +0,0 @@
-onEvent("player.tick", (event) => {
-    const player = event.getPlayer();
-    if (event.level.getBlock(player.x, player.y - 2, player.z) == "createastral:swift_andesite") {
-        player.potionEffects.add("minecraft:speed", 20, 0, false, false);
-    }
-});
