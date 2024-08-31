55a6eba026d9d4c9c0d3bae32394b02f7a0e98c5
Max Norfolk <mnorfolk@psu.edu>
2024-08-31 17:13:06 -0400

    more balancing

--- kubejs/server_scripts/machines.js
+++ kubejs/server_scripts/machines.js
@@ -60,5 +60,5 @@ onEvent('recipes', event => {
             "f": "techreborn:refined_iron_fence"
         }).requireFluid(Fluid.of("kubejs:smite", BUCKET))
         .runCommandOnEnd("/summon minecraft:lightning_bolt")
-        .runCommandOnEnd("/data modify block ~ ~3 ~ PowerAcceptor.energy set value 1000000");
+        .runCommandOnEnd("/data modify block ~ ~3 ~ PowerAcceptor.energy set value 720000");
 });
