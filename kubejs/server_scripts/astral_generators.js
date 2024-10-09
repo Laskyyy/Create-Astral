f83546c12fdc8007869fb1c9c71c71ae38c685f0
Nguyen Minh Tho <definedentity@proton.me>
2024-10-09 14:20:53 +0700

    Update recipes (1/2)

---
+++ kubejs/server_scripts/astral_generators.js
@@ -0,0 +1,56 @@
+onEvent("recipes", (event) => {
+    AGShapeRecipes(event);
+    AGShapelessRecipes(event);
+    AGSpoutRecipes(event);
+    AGMixingRecipes(event);
+});
+
+function AGMixingRecipes(event) {
+    event.recipes.create
+        .mixing("astralgenerators:fusion_glass", ["techreborn:flint_dust", "techreborn:steel_dust", "minecraft:glass"])
+        .heated();
+}
+
+function AGShapelessRecipes(event) {
+    event.shapeless("astralgenerators:vent", ["ad_astra:vent", "techreborn:basic_machine_frame"]);
+}
+
+function AGShapeRecipes(event) {
+    event.shaped("astralgenerators:multiblock_projector", ["xxx", "xya", "xxx"], {
+        x: "techreborn:basic_machine_frame",
+        y: "create:integrated_circuit",
+        a: "minecraft:light_blue_stained_glass_pane",
+    });
+    event.shaped("astralgenerators:convergence_core", ["xxx", "xyx", "xxx"], {
+        y: "techreborn:industrial_machine_frame",
+        x: "create:shadow_steel",
+    });
+    event.shaped("astralgenerators:superconducting_coil", ["xxx", "xyx", "xxx"], {
+        y: "createastral:copper_heating_coil",
+        x: "techreborn:silver_plate",
+    });
+    event.shaped("2x astralgenerators:steel_frame", ["yyy", "xxx", "xxx"], {
+        y: "minecraft:air",
+        x: "ad_astra:steel_ingot",
+    });
+    event.shaped("astralgenerators:pipe_casing", ["xxx", "xyx", "xxx"], {
+        x: "ad_astra:steel_plate",
+        y: "create:fluid_pipe",
+    });
+    event.shaped("astralgenerators:engine_intake_casing", ["xyx", "yzy", "xyx"], {
+        x: "minecraft:air",
+        y: "create:propeller",
+        z: "ad_astra:steel_block",
+    });
+    event.shaped("astralgenerators:fusion_coil", ["aaa", "axa", "yzy"], {
+        x: "techreborn:advanced_machine_frame",
+        y: "createaddition:capacitor",
+        z: "create:integrated_circuit",
+        a: "createaddition:copper_spool",
+    });
+    event.shaped("astralgenerators:fusion_casing", ["xyx", "yzy", "xyx"], {
+        x: "ad_astra:steel_plate",
+        y: "techreborn:silver_plate",
+        z: "astralgenerators:superconducting_coil",
+    });
+}
