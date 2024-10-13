5acc8b370e76a146a03ae0c962f3a43682d65bf6
ethanicuss <48843765+ethanicusss@users.noreply.github.com>
2024-10-13 18:37:18 +0100

    stone growth quest + other quests
    yea

--- kubejs/client_scripts/ponders/stone_growth_chamber.js
+++ kubejs/client_scripts/ponders/stone_growth_chamber.js
@@ -12,7 +12,7 @@ onEvent("ponder.registry", event => {
 				scene.world.setBlocks([1, 1, 1,], "minecraft:polished_andesite", true);
         scene.world.setBlocks([2, 1, 1,], "createastral:stone_growth_chamber_dummy", true);
         scene.world.setBlocks([3, 1, 1,], "minecraft:polished_andesite", true);
-        scene.text(30, "Polished Andesite", [3, 1, 3])
+        scene.text(30, "Polished Andesite", [3, 1, 5])
 				
         scene.idle(40);
         scene.world.setBlocks([1, 2, 3,], "minecraft:glass", true);
@@ -24,7 +24,7 @@ onEvent("ponder.registry", event => {
         scene.world.setBlocks([1, 2, 1,], "minecraft:glass", true);
         scene.world.setBlocks([2, 2, 1,], "minecraft:glass", true);
         scene.world.setBlocks([3, 2, 1,], "minecraft:glass", true);
-        scene.text(30, "Lava in the middle", [3, 1, 3])
+        scene.text(30, "Lava in the middle", [3, 2, 3])
 				
         scene.idle(40);
         scene.world.setBlocks([1, 3, 3,], "minecraft:polished_andesite", true);
@@ -39,7 +39,7 @@ onEvent("ponder.registry", event => {
         scene.idle(10);
         scene.text(60, "This is the Stone Growth Chamber", [3, 2.5, 3])
         scene.idle(70);
-        scene.text(80, "Use an Andesite Funnel on the front or a Hopper on the bottom to output Andesite", [2, 2.5, 1])
+        scene.text(80, "Use an Andesite Funnel on the front or a Hopper on the bottom to output Andesite", [2, 1, 1])
         scene.idle(90);
 				scene.world.setBlocks([1, 1, 3,], "ad_astra:polished_moon_stone", true);
         scene.world.setBlocks([2, 1, 3,], "ad_astra:polished_moon_stone", true);
