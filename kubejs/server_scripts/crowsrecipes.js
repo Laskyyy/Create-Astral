e62d013bb1f28cd20ee4e621b9b659c02a550c16
The Spokesman <94719433+Druivenpasta@users.noreply.github.com>
2024-10-14 13:21:20 +0200

    Make tuff great again

---
+++ kubejs/server_scripts/crowsrecipes.js
@@ -0,0 +1,23 @@
+onEvent("recipes", event => {
+    //add a use for ashes, making tuff much more relevant if you can spare the power
+    event.custom({
+        "type":"createaddition:charging",
+        "input": {
+            "item": "minecraft:tuff",
+            "count": 1
+        },
+        "result": {
+            "item": "techreborn:ashes",
+            "count": 2
+        },
+        "energy": 10000
+    })
+    event.recipes.createSplashing([
+        Item.of("minecraft:gold_nugget").withChance(0.2),
+        Item.of("create:zinc_nugget").withChance(0.225),
+        Item.of("create:copper_nugget").withChance(0.3),
+        Item.of("techreborn:tin_nugget").withChance(0.25)
+    ], [
+        "techreborn:ashes"
+    ])
+})
