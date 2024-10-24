e0929a1cd81ae6f4b1e46d9198933716d6f32277
ethanicuss <48843765+ethanicusss@users.noreply.github.com>
2024-10-25 00:03:55 +0100

    Remove Netherrack > Cinderflour
    yea

--- kubejs/server_scripts/removals.js
+++ kubejs/server_scripts/removals.js
@@ -205,6 +205,7 @@ onEvent("recipes", (event) => {
         { type: "create:crushing", input: "create:scoria" },
         { type: "create:crushing", input: "create:scorchia" },
         { type: "create:crushing", input: "minecraft:cobblestone" },
+        { type: "create:crushing", output: "create:cinder_flour" },
         { type: "create:mixing", output: "create:brass_ingot" },
         { type: "create:splashing", input: "minecraft:red_sand" },
         { type: "create:haunting", input: "minecraft:red_sand" },
