5c3acfa1d5fff8fce1db97edd76c946a310a18a7
wetmanux <168606848+wetmanux@users.noreply.github.com>
2025-02-16 22:08:16 -0600

    mushroom dye recipes
    this is mostly just for a method of brown dye without cocoa beans.
    the red dye recipe was to match
    
    red mushroom -> red dye
    brown mushroom -> brown dye

--- kubejs/server_scripts/qol_recipes.js
+++ kubejs/server_scripts/qol_recipes.js
@@ -2,4 +2,18 @@ onEvent("recipes", (event) => {
     event.shaped("4x minecraft:chest", ["XXX", "X X", "XXX"], {
         X: "#minecraft:logs",
     });
+    
+    //! mushroom dye recipes! made by wetmanux
+    event.shapeless(
+        Item.of("minecraft:brown_dye"),
+        [
+            "minecraft:brown_mushroom",
+        ]
+    );
+    event.shapeless(
+        Item.of("minecraft:red_dye"),
+        [
+            "minecraft:red_mushroom",
+        ]
+    );
 });
