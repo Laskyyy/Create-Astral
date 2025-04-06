cb867dcf64c04d8625f279cf8d230aa2d03019f5
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2025-04-06 14:20:18 +0100

    Standardise yttr/centrifuging.js

--- kubejs/server_scripts/recipes/yttr/centrifuging.js
+++ kubejs/server_scripts/recipes/yttr/centrifuging.js
@@ -1,28 +1,36 @@
-onEvent('recipes', event => {
-  event.custom({
-    type: "yttr:centrifuging",
-    time: 300,
-		ingredient: {
-			item: "minecraft:coal",
-			count: 16
-		},
-    results: [
-			{
-				item: "yttr:ultrapure_carbon",
-				count: 2
-			},
-			{
-				item: "minecraft:black_dye",
-				count: 4
-			},
-			{
-				item: "techreborn:coal_dust",
-				count: 2
-			},
-			{
-				item: "yttr:ash",
-				count: 1
-			}
-		]
-  });
-})
+onEvent("recipes", (event) => {
+    [
+        {
+            input: {
+                item: "minecraft:coal",
+                count: 16,
+            },
+            output: [
+                {
+                    item: "yttr:ultrapure_carbon",
+                    count: 2,
+                },
+                {
+                    item: "minecraft:black_dye",
+                    count: 4,
+                },
+                {
+                    item: "techreborn:coal_dust",
+                    count: 2,
+                },
+                {
+                    item: "yttr:ash",
+                    count: 1,
+                },
+            ],
+            time: 300,
+        },
+    ].forEach((recipe) => {
+        event.custom({
+            type: "yttr:centrifuging",
+            time: recipe.time,
+            ingredient: recipe.input,
+            results: recipe.output,
+        });
+    });
+});
