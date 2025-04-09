b38768cd202d5c9d570e2b67583298a4285cb3ea
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2025-04-09 12:03:29 +0100

    Had the wrong syntax in compostables.js

--- kubejs/server_scripts/recipes/minecraft/compostables.js
+++ kubejs/server_scripts/recipes/minecraft/compostables.js
@@ -5,19 +5,19 @@ onEvent("recipes", (event) => {
 
         for (const corals of coral) {
             //normal coral
-            event.add("minecraft:" + coral.type + "_coral", 0.3);
-            event.add("minecraft:dead_" + coral.type + "_coral", 0.5);
+            event.add("minecraft:" + corals.type + "_coral", 0.3);
+            event.add("minecraft:dead_" + corals.type + "_coral", 0.5);
 
             //coral fans
-            event.add("minecraft:" + coral.type + "_coral_fan", 0.3);
-            event.add("minecraft:dead_" + coral.type + "_coral_fan", 0.5);
+            event.add("minecraft:" + corals.type + "_coral_fan", 0.3);
+            event.add("minecraft:dead_" + corals.type + "_coral_fan", 0.5);
 
             //coral blocks
-            event.add("minecraft:" + coral.type + "_coral_block", 0.3);
-            event.add("minecraft:dead_" + coral.type + "_coral_block", 0.5);
+            event.add("minecraft:" + corals.type + "_coral_block", 0.3);
+            event.add("minecraft:dead_" + corals.type + "_coral_block", 0.5);
         }
         // if it breaks, blame b0b, (nah blame me :) )
         // blame b0b :0Blush:
-        // Nah blame me this time - _Shortman
+        // Actually blame me this time - _Shortman
     });
 });
