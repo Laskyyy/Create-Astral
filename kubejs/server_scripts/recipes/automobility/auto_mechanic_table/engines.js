c7d3d8cd2e078b7fcbfa6657b1ae64c4be1e9ce9
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2025-04-14 23:28:41 +0100

    Move automobility recipes to be handled by kube

---
+++ kubejs/server_scripts/recipes/automobility/auto_mechanic_table/engines.js
@@ -0,0 +1,76 @@
+onEvent("recipes", (event) => {
+    [
+        {
+            input: [
+                { item: "create:large_cogwheel" },
+                { item: "create:electron_tube" },
+                { item: "create:copper_casing" },
+                { item: "create:smart_fluid_pipe" },
+                { item: "create:gearshift" },
+                { item: "create:clutch" },
+            ],
+            output: "automobility:copper",
+            sortnum: 1,
+        },
+        {
+            input: [
+                { item: "create:integrated_circuit" },
+                { item: "minecraft:diamond_block" },
+                { item: "createaddition:alternator" },
+                { item: "createaddition:electric_motor" },
+                { item: "create:electron_tube" },
+                { item: "minecraft:diamond_block" },
+                { item: "create:rotation_speed_controller" },
+                { item: "create:mechanical_arm" },
+            ],
+            output: "automobility:diamond",
+            sortnum: 4,
+        },
+        {
+            input: [
+                { item: "create:integrated_circuit" },
+                { item: "create:brass_casing" },
+                { item: "minecraft:gold_block" },
+                { item: "techreborn:basic_machine_frame" },
+                { item: "create:cogwheel" },
+                { item: "minecraft:gold_block" },
+                { item: "create:rotation_speed_controller" },
+                { item: "create:mechanical_arm" },
+            ],
+            output: "automobility:gold",
+            sortnum: 3,
+        },
+        {
+            input: [
+                { item: "create:integrated_circuit" },
+                { item: "minecraft:iron_block" },
+                { item: "techreborn:basic_machine_frame" },
+                { item: "create:cogwheel" },
+                { item: "create:gearshift" },
+                { item: "create:clutch" },
+            ],
+            output: "automobility:iron",
+            sortnum: 2,
+        },
+        {
+            input: [
+                { item: "create:large_cogwheel" },
+                { item: "minecraft:coal_block" },
+                { item: "minecraft:furnace" },
+                { item: "create:cogwheel" },
+                { item: "create:gearshift" },
+                { item: "create:clutch" },
+            ],
+            output: "automobility:stone",
+            sortnum: 0,
+        },
+    ].forEach((recipe) => {
+        event.custom({
+            type: "automobility:auto_mechanic_table",
+            category: "automobility:engines",
+            sortnum: recipe.sortnum,
+            ingredients: recipe.input,
+            result: { item: "automobility:automobile_engine", component: recipe.output },
+        });
+    });
+});
