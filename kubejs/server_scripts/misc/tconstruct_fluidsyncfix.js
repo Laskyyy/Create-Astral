8de3351d71ded7de8e40e3f8a25b7743d597c830
KonSola5 <125081901+KonSola5@users.noreply.github.com>
2025-05-26 11:14:43 +0200

    Remove type checking altogether, assume it works

--- kubejs/server_scripts/misc/tconstruct_fluidsyncfix.js
+++ kubejs/server_scripts/misc/tconstruct_fluidsyncfix.js
@@ -1,17 +1,8 @@
 (function hephaestusFluidSyncFix() {
   onEvent("block.right_click", (event) => {
     if (event.block.id == "tconstruct:foundry_controller") {
-      let blockEntity = event.block.entity;
-      if (
-        "tank" in blockEntity &&
-        typeof blockEntity.tank == "object" &&
-        "syncFluids" in blockEntity.tank &&
-        typeof blockEntity.tank.syncFluids == "function"
-      ) {
-        blockEntity.tank.syncFluids();
-      } else {
-        throw new Error("syncFluids is not a function in the Foundry Controller's block entity.");
-      }
+      // @ts-expect-error This should always work.
+      event.block.entity?.tank?.syncFluids();
     }
   });
 })();
