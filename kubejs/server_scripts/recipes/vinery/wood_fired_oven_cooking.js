730f3dec9face711257b7c3687de0a3a5d8e367c
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2025-06-10 18:21:49 +0100

    Resolve recipe conflict with Milk Bread and Vinery Toast

---
+++ kubejs/server_scripts/recipes/vinery/wood_fired_oven_cooking.js
@@ -0,0 +1,30 @@
+(function vineryWoodFiredOvenRecipes(event) {
+  onEvent("recipes", (event) => {
+    /**
+     * @typedef WoodFiredOvenRecipe
+     * @property {Helper.ItemOrTag[]} input
+     * @property {Internal.ItemStackJS_} output
+     * @property {number} outputCount
+     * @property {number} xp
+     */
+
+    /** @type {WoodFiredOvenRecipe[]} */
+    const woodFiredOvenRecipes = [
+      {
+        input: [{ tag: "minecraft:dough" }, { tag: "minecraft:dough" }, { item: "minecraft:sugar" }],
+        output: "vinery:toast",
+        outputCount: 3,
+        xp: 0.35,
+      },
+    ];
+    woodFiredOvenRecipes.forEach((recipe) => {
+      event.custom({
+        type: "vinery:wood_fired_oven_cooking",
+        ingredients: recipe.input,
+        item: recipe.output,
+        count: recipe.outputCount,
+        experience: recipe.xp,
+      });
+    });
+  });
+})();
