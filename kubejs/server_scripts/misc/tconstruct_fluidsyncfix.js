4d792f9ed0daa62a78f485b9b8b9bf8c85dec3fd
ethanicuss <48843765+ethanicusss@users.noreply.github.com>
2025-08-11 18:10:43 +0100

    kubejs foundry warning + synthetic redstone

--- kubejs/server_scripts/misc/tconstruct_fluidsyncfix.js
+++ kubejs/server_scripts/misc/tconstruct_fluidsyncfix.js
@@ -1,8 +1,4 @@
-(function hephaestusFluidSyncFix() {
-  onEvent("block.right_click", (event) => {
-    if (event.block.id == "tconstruct:foundry_controller") {
-      // @ts-expect-error This should always work.
-      event.block.entity?.tank?.syncFluids();
-    }
-  });
-})();
+onEvent('block.right_click', event => {
+    if (event.block.id == "tconstruct:foundry_controller" && event.block.entity.tank != null)
+        event.block.entity.tank.syncFluids()
+})
