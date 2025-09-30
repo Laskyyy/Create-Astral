6fa0affa8aa020cfeeb58206b490eabb7a759331
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2025-09-30 22:48:03 +0100

    Update ponder types to work with namespaces

--- kubejs/custom_types/ponderjs.d.ts
+++ kubejs/custom_types/ponderjs.d.ts
@@ -1,27 +1,28 @@
+type Namespace = `${string}:${string}`;
+
 type TagEvent = {
-    createTag: (
-      tagName: `${string}:${string}`,
-      tagIcon: `${string}:${string}`,
-      tagTitle: string,
-      tagDescription: string,
-      defaultItems: Array<`${string}:${string}`>
-    ) => void;
-  };
-  
-  declare class PonderPalette {
-    static readonly "BLACK": Internal.PonderPalette;
-    static readonly "GREEN": Internal.PonderPalette;
-    static readonly "INPUT": Internal.PonderPalette;
-    static readonly "BLUE": Internal.PonderPalette;
-    static readonly "FAST": Internal.PonderPalette;
-    static readonly "OUTPUT": Internal.PonderPalette;
-    static readonly "MEDIUM": Internal.PonderPalette;
-    static readonly "WHITE": Internal.PonderPalette;
-    static readonly "RED": Internal.PonderPalette;
-    static readonly "SLOW": Internal.PonderPalette;
-  }
-  
-  declare function onEvent(eventName: "ponder.tag", handler: (event: TagEvent) => void): any;
-  // What's the type for the event?
-  declare function onEvent(eventName: "ponder.override", handler: (event: any) => void): any;
-  
+  createTag: (
+    tagName: Namespace,
+    tagIcon: Special.Item,
+    tagTitle: string,
+    tagDescription: string,
+    defaultItems: Namespace[]
+  ) => void;
+};
+
+declare class PonderPalette {
+  static readonly "BLACK": Internal.PonderPalette;
+  static readonly "GREEN": Internal.PonderPalette;
+  static readonly "INPUT": Internal.PonderPalette;
+  static readonly "BLUE": Internal.PonderPalette;
+  static readonly "FAST": Internal.PonderPalette;
+  static readonly "OUTPUT": Internal.PonderPalette;
+  static readonly "MEDIUM": Internal.PonderPalette;
+  static readonly "WHITE": Internal.PonderPalette;
+  static readonly "RED": Internal.PonderPalette;
+  static readonly "SLOW": Internal.PonderPalette;
+}
+
+declare function onEvent(eventName: "ponder.tag", handler: (event: TagEvent) => void): any;
+// What's the type for the event?
+declare function onEvent(eventName: "ponder.override", handler: (event: any) => void): any;
