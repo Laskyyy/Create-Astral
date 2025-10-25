344957e6851d4f6586b08916a2c42986525c18ad
ethanicuss <48843765+ethanicusss@users.noreply.github.com>
2025-10-25 23:41:08 +0100

    Update alloy_smelter.js

--- kubejs/server_scripts/recipes/techreborn/alloy_smelter.js
+++ kubejs/server_scripts/recipes/techreborn/alloy_smelter.js
@@ -17,6 +17,42 @@
         ],
         output: { item: "create:brass_ingot", count: 2 },
         power: 128,
+        time: 1200,
+      },
+	  {
+        input: [
+          { item: "minecraft:copper_ingot", count: 1 },
+          { item: "create:tin_ingot", count: 1 },
+        ],
+        output: { item: "create:bronze_ingot", count: 2 },
+        power: 48,
+        time: 600,
+      },
+	  {
+        input: [
+          { item: "minecraft:copper_ingot", count: 1 },
+          { item: "minecraft:gold_ingot", count: 1 },
+        ],
+        output: { item: "create:rose_gold_ingot", count: 2 },
+        power: 96,
+        time: 1000,
+      },
+	  {
+        input: [
+          { item: "create:andesite_alloy", count: 1 },
+          { item: "minecraft:iron_ingot", count: 1 },
+        ],
+        output: { item: "dustrial_decor:cast_iron_billet", count: 2 },
+        power: 96,
+        time: 1000,
+      },
+	  {
+        input: [
+          { item: "create:andesite_iron", count: 1 },
+          { item: "minecraft:clay", count: 1 },
+        ],
+        output: { item: "create:andesite_compound", count: 2 },
+        power: 96,
         time: 1000,
       },
     ];
