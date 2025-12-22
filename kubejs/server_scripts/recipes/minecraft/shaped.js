66471b802cb3328d5e9b2e8bff8be70785ffca64
MikoTheBoi <mikociec@gmail.com>
2025-12-22 19:23:57 +0100

    Fairly big quest changes! (will probably break other PRs)!
    Adjusted the order index in some questlines due to the new Side Quest group
    
    Changed the Threshold Switch quest to not use the Contest Observer as a dependency
    > No reason why it's like this anymore
    
    Changed the Storage Unit quest to mention Tank Units too
    > Since they're both units I decided to put them in the same quest
    
    Added new Ch3 quests (Generator, Chemical Reactor, Greenhouse and Lightning Rod)
    > Weren't mentioned before. Hopefully people know about them now!
    
    Moved some Ch3 quests to accomodate the new quests
    
    Buffed Ch4 Rocket quest reward (4 Blaze Cakes and Thruster Boots)
    > Now matches more to Ch2 and 3s rewards. Thrusters are just a nice utility
    
    Added new Ch4 quest (Sawmill)
    > The recipes are still disabled btw
    
    Buffed Iron Fence recipe (1x->2x)
    > HOLY they're expensive
    
    Added new translations for Culinary Delights!
    
    Disabled Create's Simplify Fluid Unit config (enables fractions)
    > BIG change imo. Players can finally see the real fluid amounts in assembly recipes!!

--- kubejs/server_scripts/recipes/minecraft/shaped.js
+++ kubejs/server_scripts/recipes/minecraft/shaped.js
@@ -869,7 +869,7 @@
         },
       },
       {
-        output: "techreborn:refined_iron_fence",
+        output: Item.of("techreborn:refined_iron_fence", 2),
         pattern: ["BSB", "BSB"],
         key: {
           B: "minecraft:iron_block",
@@ -1092,7 +1092,7 @@
           F: "create:blaze_burner",
         },
       },
-	  {
+      {
         output: "techreborn:alloy_smelter",
         pattern: ["ABA", "CDC", "EFE"],
         key: {
