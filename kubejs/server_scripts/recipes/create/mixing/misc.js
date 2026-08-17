34098df6f7bccf5fcdefcfda4203154c596df15f
unanimousveto <jdouglas.x@outlook.com>
2026-08-17 13:36:33 -0400

    Separated Farmer's Delight recipes
    Moved Farmer's Delight compatibility recipes out
    of the miscellaneous mixing file and into their own
    'kubejs/.../create/mixing/farmersdelight.js' file.

--- kubejs/server_scripts/recipes/create/mixing/misc.js
+++ kubejs/server_scripts/recipes/create/mixing/misc.js
@@ -2,8 +2,6 @@
   const { BUCKET, BOTTLE, GEM_BLOCK, SLIMEBALL, INGOT, GEM, NUGGET, mB } = global.fluids;
 
   onEvent("recipes", (event) => {
-    farmersCompatMixing(event);
-
     /**
      * @typedef MixingRecipe
      * @property {Internal.IngredientJS_} output
@@ -2631,57 +2629,4 @@
         .processingTime(50);
     });
   });
-  /**
-   * Farmers' Delight mixing compat recipes.
-   * @author RandomUser240306
-   * @param {Internal.RecipeEventJS_} event
-   */
-  function farmersCompatMixing(event) {
-    event.forEachRecipe({ type: "farmersdelight:cooking" }, (recipe) => {
-      let outputItem = recipe.getOriginalRecipeResult().getId().split(":")[1];
-      let inputItems = /** @type {any} */ (recipe.json.get("ingredients").deepCopy()); // Setting this to any, since I have no idea what type this really is.
-      if (outputItem == "cabbage_rolls") return; //too few ingredients to add recipe for
-      const containers = {
-        "minecraft:glass_bottle": ["hot_cocoa", "apple_cider", "glow_berry_custard"],
-        "minecraft:pumpkin": ["stuffed_pumpkin_block"],
-        "#c:dough": ["dumplings"],
-      };
-      let container =
-        /** @type {Special.Item} */ (Object.keys(containers).find((key) => containers[key].includes(outputItem))) ??
-        "minecraft:bowl";
-      for (let i = 0; i < inputItems.size(); i++) {
-        if (inputItems.get(i).getClass() == "class com.google.gson.JsonObject") {
-          if (inputItems.get(i).has("item")) {
-            if (inputItems.get(i).get("item").toString().replaceAll('"', "") == "farmersdelight:tomato_sauce") {
-              let $JsonObject = java("com.google.gson.JsonObject");
-              let tomato_sauce_fluid_ingredient = new $JsonObject();
-              tomato_sauce_fluid_ingredient.add("fluid", "kubejs:tomato_sauce_fluid");
-              tomato_sauce_fluid_ingredient.add("amount", 250 * mB);
-              inputItems.set(i, tomato_sauce_fluid_ingredient);
-            }
-          } else {
-            if (inputItems.get(i).has("tag")) {
-              if (inputItems.get(i).get("tag").toString().replaceAll('"', "") == "c:dough") {
-                inputItems.remove(inputItems.get(i)); //removes dough from dumpling recipe
-              } else if (inputItems.get(i).get("tag").toString().replaceAll('"', "") == "c:milk") {
-                let $JsonObject = java("com.google.gson.JsonObject");
-                let milk = new $JsonObject();
-                milk.add("fluid", "milk:still_milk");
-                milk.add("amount", 250 * mB);
-                inputItems.set(i, milk);
-              }
-            }
-          }
-        }
-      }
-      event.recipes
-        .createMixing({ fluid: `kubejs:${outputItem}_fluid`, amount: 250 * mB }, inputItems)
-        .heatRequirement("heated")
-        .processingTime(100);
-      event.recipes.createFilling(recipe.getOriginalRecipeResult(), [
-        container,
-        { fluid: `kubejs:${outputItem}_fluid`, amount: 250 * mB },
-      ]);
-    });
-  }
 })();
